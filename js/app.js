d6badc4434d451345d831bb816c49f07141eabfd
Wei-Jen <weijen0330@hotmail.com>
2015-11-19 15:22:35 -0800

    completed lab 11/19

--- js/app.js
+++ js/app.js
@@ -7,6 +7,32 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext('2d');
+    var mouseIsDown = false;
+    var colorInp = document.querySelector('#line-color-inp');
 
+    canvas.addEventListener('mousedown', function(evt) {
+        mouseIsDown = true;
+        ctx.beginPath();
+        ctx.moveTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        if (mouseIsDown) {
+            ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        mouseIsDown = false;
+        ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+        ctx.stroke();
+    });
+
+    colorInp.addEventListener('change', function(evt) {
+        ctx.strokeStyle = colorInp.value;
+    })
 
 }); //DOMContentLoaded
